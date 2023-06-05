00e3544dbb4a30bc456068ec4ddd392ec0d5cb1a
thikimthuy51 <20522004@gm.uit.edu.vn>
2023-06-06 02:07:16 +0700

    update_UI

--- music_player_website/src/components/DetailSong.js
+++ music_player_website/src/components/DetailSong.js
@@ -5,14 +5,14 @@ export default function DetailSong() {
     const {song} = useContext(Songs)
     return (
         <div className='col-span-1 p-3'>
-            <h2 className='text-cyan-500 font-bold'>Bài hát đang phát</h2>
-            <h2 className='text-neutral-400 text-2xl'>{song.name}</h2>
-            <div className='w-[160px] m-auto mt-10'>
+            <h2 className='text-purple-50 font-bold'>Bài hát đang phát</h2>
+            <h2 className='text-green-50 text-2xl'>{song.name}</h2>
+            <div className='w-[190px] m-auto mt-10'>
                 <img className='w-full' src={song.links.images[0].url} alt='avatar' />
             </div>
-            <div className='flex justify-evenly items-center mt-10'>
+            <div className='flex justify-center items-center mt-5'>
             <img className='w-[60px] rounded-full' src={song.links.images[1].url} alt='avatar' />
-            <span className='text-xl text-white'>{song.author}</span>
+            <span className='text-xl text-white px-2 '>{song.author}</span>
             </div>
         </div>
     )
--- music_player_website/src/components/ListSongs.js
+++ music_player_website/src/components/ListSongs.js
@@ -22,17 +22,17 @@ export default function ListSongs() {
             <th className="w-[10%]">Tải về</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="listsong">
           {DataSongs.map((song, index) => (
             <tr
               key={index}
-              className={`bg-slate-800 h-12 text-gray-500 hover:bg-slate-600 ${idSong === song.id && 'bg-slate-600 text-teal-400'}`}
+              className={`h-12 text-gray-100 hover:bg-purple-500 ${idSong === song.id && 'bg-purple-600 text-green-400'}`}
               onClick={() => handlePlaySong(song.id)}
             >
               <td className="text-center">{index + 1}</td>
               <td>{song.name}</td>
               <td className="text-center">{song.author}</td>
-              <td className="text-center hover:text-gray-200">
+              <td className="text-center hover:text-purple-900">
                 <a href={song.url}>
                   <i className="fa fa-download"></i>
                 </a>
